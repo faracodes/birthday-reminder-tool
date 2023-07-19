dd42ac803c7b3edbd859bd004cc517e6151505f5
Fara <fara@pc-195-97.customer.ask4.lan>
2023-07-19 19:52:10 +0100

    distance-between-now-and-the-new-year

--- project.py
+++ project.py
@@ -8,3 +8,10 @@ current_time = datetime.datetime.now()
 
 # Printing value of now.
 print("Time now at greenwich meridian is:", current_time)
+
+#Printing out the distance between 19/07/23 and 01/01/24 
+from datetime import date
+f_date = date(2023, 7, 19)
+l_date = date(2024, 1, 1)
+delta = l_date - f_date
+print(delta.days)
